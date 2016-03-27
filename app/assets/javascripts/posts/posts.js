f143cd8316aa908a9c5af0206b37f911a71fd55c
Chris <chris.haack@me.com>
2016-03-27 11:22:14 -0400

    automatically load post with resolve object

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -10,6 +10,11 @@ angular.module('flapperNews')
         angular.copy(data, o.posts);
       });
     };
+    o.get = function(id) {
+      return $http.get('/posts/' + id + '.json').then(function(res) {
+        return res.data;
+      });
+    };
     o.create = function(post) {
       return $http.post('/posts.json', post).success(function(data) {
         o.posts.push(data);
