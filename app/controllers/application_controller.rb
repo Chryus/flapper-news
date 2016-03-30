4aba0e737f92acff3832bc73ad32486b96b2118a
Chris <chris.haack@me.com>
2016-03-29 23:11:50 -0400

    set csrf token for all angular requests to avoid 422 invalid auth token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,26 @@ class ApplicationController < ActionController::Base
   respond_to :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :set_csrf_cookie_for_ng
+
+  # rescue_from ActionController::InvalidAuthenticityToken do |exception|
+  #   cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  #   render :error => 'invalid token', {:status => :unprocessable_entity}
+  # end
+
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+  end
 
   def angular
     render 'layouts/application'
   end
 
   private
+
+  def verified_request?
+    super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :username
